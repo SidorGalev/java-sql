1f8089817a61bfdd1a436f8fc5e32d9de243fb1d
SidorGalev <sidor_2004@abv.bg>
2023-03-31 09:36:22 +0300

    sql proekt

---
+++ src/java/LoginServlet.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+  protected void doGet(
+          HttpServletRequest request, 
+          HttpServletResponse response)
+            throws ServletException, IOException {
+      PrintWriter writer = response.getWriter();
+      String user = request.getParameter("user");
+      String pass = request.getParameter("pass");
+      if (user.equals ("Sidor")
+        && pass.equals ("1234")) {
+          writer.println("Login Successful");
+      } else { 
+                writer.println("Login failed");
+
+      }
+      
+    }
+}
